5a4cf0a936056acaca7d6d06d4d7e9d1ec932a0e
anton <anton@REALMEBOOK>
2024-01-31 13:33:12 +0700

    Добавьте файлы проекта.

---
+++ SocialNetwork/PLL/Views/AddFriendView.cs
@@ -0,0 +1,49 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AddFriendView
+    {
+        UserService userService;
+        public AddFriendView(UserService userService)
+        {
+            this.userService = userService;
+        }
+        public void Show(User user)
+        {
+             try
+            {
+                var friend = new Friend();
+
+                Console.WriteLine("Введите Email пользователя, которого хотите добавить в друзья:\n");
+
+                friend.FriendEmail = Console.ReadLine();
+                friend.UserId = user.Id;
+
+                userService.AddFriend(friend);
+
+                SuccessMessage.Show("Вы добавили пользователя в друзья!");
+            }
+
+            catch(UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователя с таким Email не существует!");
+            }
+            
+            catch(Exception)
+            {
+                AlertMessage.Show("Ошибка при добавлении пользователя в друзья!");
+            }
+
+        }
+
+    }
+}
---
+++ SocialNetwork/PLL/Views/UserFriendView.cs
@@ -0,0 +1,32 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class UserFriendView
+    {
+        public void Show(IEnumerable<User> friends)
+        {
+            Console.WriteLine("\nДрузья:\n");
+
+            if (friends.Count() == 0)
+            {
+                Console.WriteLine("Список друзей пуст");
+                return;
+            }
+
+            foreach (var friend in friends)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine($"Почтовый адрес: {friend.Email}");
+                Console.WriteLine($"Имя: {friend.FirstName}");
+                Console.WriteLine($"Фамилия: {friend.LastName}");
+                Console.WriteLine("---\n");
+            }
+        }
+    }
+}
